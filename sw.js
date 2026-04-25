7d43c822bfbb3be31fb2a659c31a87d6956907c0
ziyadzaki10ok <ziyadzaki10ok@gmail.com>
2026-04-25 13:56:31 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'sudoku-v1';
+const FILES = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
